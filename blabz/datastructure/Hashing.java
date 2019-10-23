219700d808e130366ef1fa356b570522fc5cd1e2
AmarArjunGunjal <amargunjal28@gmail.com>
2019-10-23 19:27:09 +0530

    23/10 evening

--- blabz/datastructure/Hashing.java
+++ blabz/datastructure/Hashing.java
@@ -9,48 +9,6 @@ import com.blabz.Utility.Utility;
 
 public class Hashing {
 public static void main(String[] args) throws IOException {
-//	Utility utility=new Utility();
-//	System.out.println("Enter Size of hashtable");
-//	int size=utility.ReadIntegers();
-//	Utility table[] = new Utility[size];
-//	Utility writefile = new Utility();
-//
-//	String path = "/home/user/eclipse-workspace/com.BridgeLabz/src/com/BridgeLabz/DataStructurePrograms/number.txt";
-//	String[] content_of_file = utility.readFile(path);
-//	System.out.println(content_of_file[0].length());
-//
-//	for (int j = 0; j < content_of_file.length; j++) {
-//		( writefile).addToHashTable(table, Integer.parseInt(content_of_file[j]));
-//	}
-//	System.out.println("Enetr number to be searched");
-//	int searchkey = utility.ReadIntegers();
-//	writefile.addToHashTable(table, searchkey);
-//
-//	for (int j = 0; j < 11; j++) {
-//		System.out.println();
-//		try {
-//			table[j].display();
-//		} catch (Exception e) {
-//			System.out.print("null");
-//		}
-//	}
-//	for(int i=0;i<11;i++)
-//	{ 
-//		try {
-//			int size1 = table[i].size();
-//			for(int j=0;j<size1;j++)
-//			{
-//				writefile.addOrdered((int)table[i].pop());
-//			}
-//		}
-//		catch(Exception e)
-//		{
-//			continue;
-//		}
-//	}
-//	System.out.println();
-//	writefile.display();
-//	writefile.writeIntoFile(path);
-//	 
+
 }
 }
