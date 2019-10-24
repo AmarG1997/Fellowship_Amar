d02c8a0e6803762eda85506032d1539a77b480b8
AmarArjunGunjal <amargunjal28@gmail.com>
2019-10-24 12:47:55 +0530

    24/10 afternoon

--- blabz/datastructure/PrimeeTwoDim.java
+++ blabz/datastructure/PrimeeTwoDim.java
@@ -12,20 +12,41 @@ public class PrimeeTwoDim {
 //		System.out.println(prime[i]);
 //	}
 		int k = 0;
-		int count=0;
+		int count;
 		int[] temp = new int[200];
 		int arr[][] = new int[10][];
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = k; j < k + 100; j++) {
-				if (prime[j] < k + 100) {
+		int p=100;
+		int j=0;
+		
+		for (int i = 0; i < arr.length; i++)
+		{
+			count=0;
+			while(j<=p+100)
+			{
+				if(prime[j]>0)
+				{
+					temp[count]=j;
 					count++;
-					temp[j] = j;
 				}
+				j++;
 			}
-			k = k + 100;
-			
+			p=p+100;
+			j=p;
 			arr[i]=new int[count];
-			System.out.println();
+			for(k=0;k<count;k++)
+			{
+				
+			}
+//			for (int j = k; j < k + 100; j++) {
+//				if (prime[j] < k + 100) {
+//					count++;
+//					temp[j] = j;
+//				}
+//			}
+//			k = k + 100;
+//			
+//			arr[i]=new int[count];
+//			System.out.println();
 		}
 	}
 
